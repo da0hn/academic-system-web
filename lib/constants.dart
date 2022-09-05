13215e2e734ce12b5bf7c5d1297396cc90d23890
Da0hn <gabrielhondacba@gmail.com>
2022-09-05 14:41:36 -0400

    feat: add docs to `Constraints` attributes 📝

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,18 @@
 class Constraints {
+  /// constant padding height/width 24
+  static const double padding24 = 24.0;
+
+  /// constant padding height/width 16
   static const double padding16 = 16.0;
+
+  /// constant padding height/width 8
   static const double padding8 = 8.0;
-  static const double radius = 10;
+
+  /// constant radius 16
+  static const double radius = 16.0;
+
+  /// constant elevation 16
+  static const double elevation = 16.0;
 }
 
 class Routes {
