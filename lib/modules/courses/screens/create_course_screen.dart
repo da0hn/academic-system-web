bc949c7cf2157295c71fc47a7ab08db1b57c2c91
Da0hn <gabrielhondacba@gmail.com>
2022-09-05 14:45:34 -0400

    wip: add base layout of create course screen

--- lib/modules/courses/screens/create_course_screen.dart
+++ lib/modules/courses/screens/create_course_screen.dart
@@ -1,3 +1,5 @@
+import 'package:academic_system/components/shared/base_layout.dart';
+import 'package:academic_system/constants.dart';
 import 'package:flutter/material.dart';
 
 class CreateCourseScreen extends StatelessWidget {
@@ -5,6 +7,67 @@ class CreateCourseScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: BaseLayout(
+        child: IntrinsicHeight(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(flex: 1),
+              Expanded(
+                flex: 8,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: Constraints.padding24),
+                    Expanded(
+                      flex: 6,
+                      child: Card(
+                        elevation: Constraints.elevation,
+                        color: Theme.of(context).primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            Constraints.radius,
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text('TESTE'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: Constraints.padding24),
+                  ],
+                ),
+              ),
+              const Spacer(flex: 1),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material _card(BuildContext context) {
+    return Material(
+      elevation: 16,
+      borderRadius: const BorderRadius.all(
+        Radius.circular(25),
+      ),
+      child: Container(
+        constraints: const BoxConstraints.expand(),
+        clipBehavior: Clip.hardEdge,
+        padding: const EdgeInsets.all(
+          Constraints.padding16,
+        ),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(25),
+          ),
+        ),
+      ),
+    );
   }
 }
